f228e8c9ad76d7324355e767a5fd753a517df96a
lijingtang <lijtforjava@sina.com>
2020-01-17 15:26:04 +0800

    mandatory

--- src/main/java/com/ljt/study/rabbitmq/client/MandatoryTest.java
+++ src/main/java/com/ljt/study/rabbitmq/client/MandatoryTest.java
@@ -22,28 +22,25 @@ public class MandatoryTest {
     }
 
 
-    private static final String QUEUE_NAME = "test.client.mandatory";
     private static final String EXCHANGE_NAME = "test.direct.mandatory";
 
     private static class Producer {
 
         static void send() throws Exception {
-            try (Channel channel = RabbitMQUtils.getChannel()) {
-                if (Objects.isNull(channel)) {
-                    System.out.println("Channel is null.");
-                    System.exit(-1);
-                }
-
-                channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-                channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-                channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "aaaaa");
-                String message = "Hello World!";
-                channel.addReturnListener((replyCode, replyText, exchange, routingKey, properties, body) ->
-                        System.out.println(new String(body, StandardCharsets.UTF_8) + "未投递成功 routingKey=" + routingKey));
-
-                channel.basicPublish(EXCHANGE_NAME, "xxx", true, MessageProperties.PERSISTENT_TEXT_PLAIN,
-                        message.getBytes());
+            Channel channel = RabbitMQUtils.getChannel();
+            if (Objects.isNull(channel)) {
+                System.out.println("Channel is null.");
+                System.exit(-1);
             }
+
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+            String message = "Hello World!";
+            channel.addReturnListener((replyCode, replyText, exchange, routingKey, properties, body) ->
+                    System.out.println(new String(body, StandardCharsets.UTF_8) + "未投递成功 routingKey=" + routingKey));
+
+            channel.basicPublish(EXCHANGE_NAME, "unbind-key", true, MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes());
         }
     }
 
